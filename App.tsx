a6d7089ee5f2d2ec0ab427989f274cfeb1a5ef80
Marios Kontis <kontism@gmail.com>
2022-06-28 14:55:41 +0100

    Create signin and signup

--- App.tsx
+++ App.tsx
@@ -2,15 +2,26 @@ import {StatusBar} from 'expo-status-bar';
 import {StyleSheet, View} from 'react-native';
 import '@aws-amplify/ui-react/styles.css';
 import Loading from './src/components/Loading';
+import { Amplify, Auth } from 'aws-amplify';
+import awsconfig from './src/aws-exports';
+import { withAuthenticator } from '@aws-amplify/ui-react';
 
+Amplify.configure(awsconfig);
 
 // eslint-disable-next-line require-jsdoc
-export default function App() {
+const App = (props: any) => {
+  let user: any = props.user
+  let signOut: any = props.signOut
+
   return (
+    <>
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Loading />
+      <h1>Hello {user.attributes.email.split('@')[0]}</h1>
+      <button onClick={signOut}>Sign out</button>
     </View>
+    </>
   );
 }
 
@@ -22,3 +33,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default withAuthenticator(App);
