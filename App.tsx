5add42732bca2753e0f0e431c7c1d49609b8a192
Kyle McPhail <99103416+MrMcSnail@users.noreply.github.com>
2022-06-28 10:44:20 +0100

    branch init for workflow sharing

--- App.tsx
+++ App.tsx
@@ -1,16 +1,15 @@
 import {StatusBar} from 'expo-status-bar';
 import {StyleSheet, View} from 'react-native';
-
-import {Authenticator} from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+import Loading from './src/components/Loading';
 
 
 // eslint-disable-next-line require-jsdoc
 export default function App() {
   return (
     <View style={styles.container}>
-      <Authenticator hideDefault={true}/>
       <StatusBar style="auto" />
+      <Loading />
     </View>
   );
 }
---
+++ src/components/Loading.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+
+const Loading = () => {
+  return (
+    <View>
+      <Text>Loading</Text>
+    </View>
+  );
+};
+
+export default Loading;
