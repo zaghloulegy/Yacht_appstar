e5fd9a94a8d90651a4adb79a4d0e8b26decb29b4
Kyle McPhail <99103416+MrMcSnail@users.noreply.github.com>
2022-06-28 11:23:20 +0100

    this commit creates a basic loading component for rendering as needed

--- App.tsx
+++ App.tsx
@@ -17,7 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1D3557',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,9 +1,23 @@
-import {View, Text} from 'react-native';
+/* eslint-disable max-len */
+import {View, Text, Image, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 150,
+    height: 150,
+  },
+  baseText: {
+    textAlign: 'center',
+    color: '#ffffff',
+    fontSize: 32,
+  },
+});
 
 const Loading = () => {
   return (
     <View>
-      <Text>Loading</Text>
+      <Image source={require('../../assets/Loading-Badge.png')} style={styles.logo}/>
+      <Text style={styles.baseText}>Loading</Text>
     </View>
   );
 };
