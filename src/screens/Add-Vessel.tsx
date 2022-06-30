0043717feab32da39ca4006fd7757b4fbf4cff87
Kyle McPhail <99103416+MrMcSnail@users.noreply.github.com>
2022-06-30 17:13:21 +0100

    this commit adds Async Storage to store user data. adds prototype storage functionality to add vessel.

--- src/screens/Add-Vessel.tsx
+++ src/screens/Add-Vessel.tsx
@@ -1,21 +1,43 @@
-import {View, Text, Button, TextInput} from 'react-native';
-import React from 'react';
+/* eslint-disable require-jsdoc */
+import {View, Text, TextInput} from 'react-native';
+import React, {useState} from 'react';
 import Footer from '../components/Footer';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+interface VesselInterface {
+  mmsi: number;
+  name: string;
+}
+const vessel: VesselInterface = {mmsi: 0, name: ''};
 
 const AddVessel = () => {
+  const [newVessel, setNewVessel]= useState(vessel);
+
+  const handleAddVessel = async (event: any) => {
+    vessel.mmsi = event.target.value;
+    setNewVessel(vessel);
+    try {
+      const vesselStringify = JSON.stringify(newVessel);
+      await AsyncStorage.setItem(`${newVessel.mmsi}`, vesselStringify);
+      console.log('out of storage', AsyncStorage.getItem(`${newVessel.mmsi}`));
+    } catch (err) {
+      console.log('err: ', err);
+    }
+  };
+
+  
   return (
     <View style={
       {flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
         flex: 1,
-        backgroundColor: '#1D3557'
+        backgroundColor: '#1D3557',
       }
     }>
-      <Text style={{color:'#F1FAEE',fontWeight:'600',fontSize: 30}}>Add Vessel</Text>
+      <Text style={{color: '#F1FAEE', fontWeight: '600', fontSize: 30}}>Add Vessel</Text>
 
-      <TextInput style={{backgroundColor:'#F1FAEE',padding:2}} placeholder='MMSI Number' placeholderTextColor="black"/>
-      <Button title="Submit"></Button>
+      <TextInput onSubmitEditing={handleAddVessel} style={{backgroundColor: '#F1FAEE', padding: 2}} placeholder='MMSI Number' placeholderTextColor="black"/>
       <Footer />
     </View>
   );
