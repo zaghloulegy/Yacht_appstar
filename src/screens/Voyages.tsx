56c05d8da754eddbd6037c6e8675759088453ee5
Oathian <aidanmaycock@gmail.com>
2022-07-04 11:38:13 +0100

    voyages renders voyages

--- src/screens/Voyages.tsx
+++ src/screens/Voyages.tsx
@@ -1,14 +1,38 @@
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, TouchableOpacity} from 'react-native';
 import React from 'react';
 import {useNavigation} from '@react-navigation/native';
 import Footer from '../components/Footer';
+import {useEffect, useState} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 type StackParamList = {
   navigate: any;
 };
 
+let initialRenderVessels: any;
+
 const Voyages = () => {
   const navigation = useNavigation<StackParamList>();
+  const [voyages, setVoyages] = useState(initialRenderVessels);
+  const [addVoyages, setAddVoyages] = useState(false);
+
+
+  useEffect(() => {
+    const renderVoyages = async () => {
+      try{
+        setAddVoyages(false);
+        const allKeys = await AsyncStorage.getAllKeys();
+        let filterKeys = allKeys.filter((key:any) => {
+          return  key.includes('voyage');
+        })
+        let allVoyages = await AsyncStorage.multiGet(filterKeys);
+        setVoyages(allVoyages);
+      } catch (err) {
+        console.log('err: ', err);
+      }
+    };
+    renderVoyages();
+  }, [addVoyages])
 
   return (
     <View style={
@@ -20,8 +44,24 @@ const Voyages = () => {
       }
     }>
       <Text style={{color:'#F1FAEE',fontWeight:'600',fontSize: 30}}>Voyages</Text>
-      
+
       <Button title="Latest Nav Log" onPress={() => navigation.navigate('Nav Log')}></Button>
+
+      {voyages?voyages.map((voyage: any) => {
+        console.log(voyage)
+        const individualMMSI = JSON.parse(voyage[1]).mmsi;
+        const individualStart = JSON.parse(voyage[1]).start_at_sea;
+        const individualEnd = JSON.parse(voyage[1]).end_at_sea;
+
+        return (
+          <View key={voyage[0]}>
+          <TouchableOpacity key={voyage[0]}>
+            <Text style={{borderWidth: 1, padding: 20,backgroundColor: '#A8DADC', borderRadius: 2, borderColor: '#black', borderBottomWidth: 0, shadowColor: 'rgba(1,1,0,0.1)', shadowOffset: {width: 3, height: 20}, shadowOpacity: 0.8, shadowRadius: 15,elevation: 2,marginLeft: 5, marginRight: 5, marginTop: 10,}} key={voyage[0]}>MMSI:{individualMMSI}, start:{individualStart}, end:{individualEnd}</Text>
+            <Button title="Make call"></Button>
+          </TouchableOpacity>
+          </View>
+        )
+      }):<></>}
       <Footer />
     </View>
   );
