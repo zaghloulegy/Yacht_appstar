8eeb2ef2cc457a94fc85a54ce8fa0990448444cd
Marios Kontis <kontism@gmail.com>
2022-07-06 00:01:38 +0100

    Cypress setup

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,8 @@ import {defineConfig} from 'cypress';
 
 export default defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:19006',
+    // iPhone 12 Pro screen dimensions
     viewportHeight: 844,
     viewportWidth: 390,
     setupNodeEvents(on, config) {
