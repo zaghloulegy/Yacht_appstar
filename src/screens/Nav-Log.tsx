b65016f544b19ff08645955aeceb72551a8db166
Kyle McPhail <99103416+MrMcSnail@users.noreply.github.com>
2022-07-06 14:57:22 +0100

    layout work on nav-log

--- src/screens/Nav-Log.tsx
+++ src/screens/Nav-Log.tsx
@@ -15,7 +15,7 @@ const NavLog = () => {
 
   useEffect(() => {
     const renderVoyages = async () => {
-      try{
+      try {
         setAddVoyages(true);
         const allKeys = await AsyncStorage.getAllKeys();
         const filterKeys = allKeys.filter((key:any) => {
@@ -29,7 +29,7 @@ const NavLog = () => {
           setTotalNighthours(totalNighthours + parseFloat(parsedData.voyageReport.nightHours));
           setTotalInCommand(totalInCommand + parseFloat(parsedData.voyageReport.daysInCommand));
           setTotalDaysAtSea(totalDaysAtSea + parseFloat(parsedData.voyageReport.voyageDistance));
-        })
+        });
       } catch (err) {
         console.log('err: ', err);
       }
@@ -43,44 +43,54 @@ const NavLog = () => {
         alignItems: 'center',
         justifyContent: 'center',
         flex: 1,
-        backgroundColor: '#1D3557'
+        backgroundColor: '#1D3557',
       }
     }>
-    <View style={{position:'absolute',top: 10,flexDirection: 'column', alignItems: 'center',justifyContent: 'center',flex: 1,}}>
-      <Text style={{color:'#F1FAEE',fontWeight:'600',fontSize: 30}}>Nav Log</Text>
+      <View style={{position: 'absolute', top: 10, flexDirection: 'column', alignItems: 'center', justifyContent: 'center', flex: 1}}>
+        <Text style={{color: '#F1FAEE', fontWeight: '600', fontSize: 30}}>Nav Log</Text>
 
-    <View style={{backgroundColor: '#A8DADC',padding:10}}>
-      <Text>Total Distance:{totalDistance}Nm Total Nighthours:{totalNighthours}h</Text>
-      <Text>Total Days In Command:{totalInCommand} Total Days at Sea:{totalDaysAtSea}</Text>
-    </View>
+        <View style={{backgroundColor: '#A8DADC', padding: 10}}>
+          <Text>Total Distance:{totalDistance}Nm Total Nighthours:{totalNighthours}h</Text>
+          <Text>Total Days In Command:{totalInCommand} Total Days at Sea:{totalDaysAtSea}</Text>
+        </View>
 
-    <TouchableOpacity style={{borderWidth: 1, padding: 20,backgroundColor: '#A8DADC', borderRadius: 10, borderColor: '#black', borderBottomWidth: 0, shadowColor: 'rgba(1,1,0,0.1)', shadowOffset: {width: 3, height: 20}, shadowOpacity: 0.8, shadowRadius: 15,elevation: 2,marginLeft: 5, marginRight: 5, marginTop: 10,}}>
-      <Text>Export Nav Log</Text>
-    </TouchableOpacity>
+        <TouchableOpacity style={{borderWidth: 1, padding: 20, backgroundColor: '#A8DADC', borderRadius: 10, borderColor: '#black', borderBottomWidth: 0, shadowColor: 'rgba(1,1,0,0.1)', shadowOffset: {width: 3, height: 20}, shadowOpacity: 0.8, shadowRadius: 15, elevation: 2, marginLeft: 5, marginRight: 5, marginTop: 10}}>
+          <Text>Export Nav Log</Text>
+        </TouchableOpacity>
 
-    <ScrollView style={{height:600,margin:10}}>
-      {voyages?voyages.map((voyage:any) => {
-        const parsedData = JSON.parse(voyage[1]);
-        console.log(parsedData)
-        return (<View key={voyage[0]}>
-          {parsedData.voyageData.positions.map((position:any) => {
-          return(
-          <View style={{backgroundColor:'#A8DADC',margin:4}} key={position.last_position_UTC}>
-            <Text>MMSI: {parsedData.mmsi}</Text>
-            <Text>Heading: {position.heading}</Text>
-            <Text>Course: {position.course}</Text>
-            <Text>Lon: {position.lon}</Text>
-            <Text>Lat: {position.lat}</Text>
-            <Text>Date: {position.last_position_UTC}</Text>
-            <Text>Role: </Text>
-          </View>);
-        })}
-        </View>)
-      }):<></>}
-    </ScrollView>
-      
-    </View>
-      <View style={{position:'absolute',bottom:10,}}>
+        <View style={{width: '100%', justifyContent: 'space-between', flexDirection: 'row', backgroundColor: '#A8DADC', margin: 4}}>
+          <Text style={{borderColor: '#F1FAEE'}}>MMSI</Text>
+          <Text>Heading</Text>
+          <Text>Course</Text>
+          <Text>Lon</Text>
+          <Text>Lat</Text>
+          <Text>Date</Text>
+          <Text>Role</Text>
+        </View>;
+
+        <ScrollView style={{width: '100%', height: 600, margin: 10}}>
+          {voyages?voyages.map((voyage:any) => {
+            const parsedData = JSON.parse(voyage[1]);
+            console.log(parsedData);
+            return (<View key={voyage[0]}>
+              {parsedData.voyageData.positions.map((position:any) => {
+                return (
+                  <View style={{flexShrink: '7', width: '100%', justifyContent: 'space-between', flexDirection: 'row', backgroundColor: '#A8DADC', margin: 4}} key={position.last_position_UTC}>
+                    <Text style={{fontSize: 10}}>{parsedData.mmsi}</Text>
+                    <Text style={{fontSize: 10}}>{position.heading}</Text>
+                    <Text style={{fontSize: 10}}>{position.course}</Text>
+                    <Text style={{fontSize: 10}}>{position.lon.toFixed(2)}</Text>
+                    <Text style={{fontSize: 10}}>{position.lat.toFixed(2)}</Text>
+                    <Text style={{fontSize: 10}}>{position.last_position_UTC}</Text>
+                    <Text style={{fontSize: 10}}>Role: </Text>
+                  </View>);
+              })}
+            </View>);
+          }):<></>}
+        </ScrollView>
+
+      </View>
+      <View style={{position: 'absolute', bottom: 10}}>
         <Footer />
       </View>
     </View>
